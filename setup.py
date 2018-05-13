d629a1d3ab66ab1e114f689b6ea9ee86f495f960
Kangbo Li <kli89@wisc.edu>
2018-05-13 16:31:23 -0500

    Add setup and license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='sfqlib',
+    packages=['sfqlib'],
+    version='0.1',
+    description='Tools for SFQ research',
+    author='Kangbo Li',
+    author_email='kli89@wisc.edu',
+    url='https://github.com/hannoeichel/mypackage',
+    keywords=['SFQ', 'Control'],
+    license='MIT',
+    classifiers=[],
+)
