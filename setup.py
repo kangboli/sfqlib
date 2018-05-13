3c9e1c4a1502ee462e6fa7b056822bbfa2dae3f6
Kangbo Li <kli89@wisc.edu>
2018-05-13 17:04:38 -0500

    Add wheel

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
+long_description = "This package contains tools for simulating qubit control with Single Flux Quantum Pulse(SFQ) Trains. This package is under development. Contributions are welcome."
 setup(
     name='sfqlib',
     packages=['sfqlib'],
     version='0.1',
     description='Tools for SFQ research',
+    long_description=long_description,
     author='Kangbo Li',
     author_email='kli89@wisc.edu',
     url='https://github.com/hannoeichel/sfqlib',
     keywords=['SFQ', 'Control'],
-    install_requires=['numpy', 'scipy', 'unittest'],
+    install_requires=['numpy', 'scipy', 'unittest', ],
     license='MIT',
     classifiers=[],
 )
