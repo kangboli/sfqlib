3ce73daebf8f592233d4f7b0ec3ffab685cc094a
Kangbo Li <kli89@wisc.edu>
2018-05-13 17:11:58 -0500

    Fix setup description

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
-long_description = "This package contains tools for simulating qubit control with Single Flux Quantum Pulse(SFQ) Trains. This package is under development. Contributions are welcome."
 setup(
     name='sfqlib',
     packages=['sfqlib'],
     version='0.1',
     description='Tools for SFQ research',
-    long_description=long_description,
+    long_description="This package contains tools for simulating qubit control with Single Flux Quantum Pulse(SFQ) Trains. This package is under development. Contributions are welcome.",
     author='Kangbo Li',
     author_email='kli89@wisc.edu',
     url='https://github.com/hannoeichel/sfqlib',
