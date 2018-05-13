52a86b5830978fca3ab1b68a95f52588c24490b0
Kangbo Li <kli89@wisc.edu>
2018-05-13 17:50:00 -0500

    Add euler.pdf.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 setup(
     name='sfqlib',
     packages=['sfqlib'],
-    version='0.1',
+    version='0.1.1',
     description='Tools for SFQ research',
     long_description="This package contains tools for simulating qubit control with Single Flux Quantum Pulse(SFQ) Trains. This package is under development. Contributions are welcome.",
     author='Kangbo Li',
     author_email='kli89@wisc.edu',
+    package_data={'sfqlib': ['euler.pdf'], },
     url='https://github.com/hannoeichel/sfqlib',
     keywords=['SFQ', 'Control'],
     install_requires=['numpy', 'scipy', 'unittest', ],
